9828e6f61927ee5c9f3190c50866f3c5e2f7dadc
dharshramakrishnan <priyaramakrishnan1629@gmail.com>
2026-09-21 23:34:39 +0530

    removed unsused imports

--- sources/java/src/main/java/com/dharsh/Payments/Service/AccountService.java
+++ sources/java/src/main/java/com/dharsh/Payments/Service/AccountService.java
@@ -9,7 +9,7 @@ import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 import com.dharsh.Payments.DTOs.AccountStatusOperation;
@@ -17,7 +17,7 @@ import com.dharsh.Payments.DTOs.AmountDeposit;
 import com.dharsh.Payments.DTOs.AmountWithdrawal;
 import com.dharsh.Payments.DTOs.CreateAccountRequests;
 import com.dharsh.Payments.Exceptions.AccountNotFoundException;
-import com.dharsh.Payments.Exceptions.AccountNotFoundException;
+
 import com.dharsh.Payments.Exceptions.CustomerNotFoundException;
 import com.dharsh.Payments.Model.AccountModel;
 import com.dharsh.Payments.Model.AccountStatus;
